61a162380422109f1a548b7b01107efc923d6188
Timo Renk <timo.renk@gmail.com>
2024-07-24 21:42:21 +0200

    feat: add for-loop and iter implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
 #![allow(unused)]
 
 fn main() {
-    println!("Hello World!!!");
+    let arr = [4, 9, 23, 12, 5, 10, 24, 16];
+    let mut vec = Vec::with_capacity(arr.len());
+    println!("### for-loop: {arr:?} ###");
+    for i in arr {
+        println!("outer, el: {i}");
+        if i > 10 {
+            println!("middle, el > 10: {i}");
+            if i % 2 == 0 {
+                println!("inner, el % 2: {i}");
+                let i = i + 1;
+                vec.push(i);
+            }
+        }
+    }
+    println!("vec: {vec:?}");
+    println!("### iter: {arr:?} ###");
+    let iter_vec: Vec<_> = arr
+        .iter()
+        .filter(|&i| {
+            println! {"outer, el: {i}"};
+            *i > 10
+        })
+        .filter(|&i| {
+            println!("middle, el > 10: {i}");
+            i % 2 == 0
+        })
+        .map(|i| {
+            println!("inner, el % 2: {i}");
+            i + 1
+        })
+        .collect();
+    assert_eq!(vec, iter_vec);
 }
