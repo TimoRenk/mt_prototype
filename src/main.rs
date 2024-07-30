b743445e745ef78c05b7ffa354626cb88eb191cf
Timo Renk <timo.renk@gmail.com>
2024-07-30 22:13:24 +0200

    wip: update main for testing

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,61 @@
 #![allow(unused)]
 
+// const ARR: [u32; 8] = [4, 9, 23, 12, 5, 10, 24, 16];
 fn main() {
-    let arr = [4, 9, 23, 12, 5, 10, 24, 16];
-    let mut vec = Vec::with_capacity(arr.len());
-    println!("### for-loop: {arr:?} ###");
-    for i in arr {
-        println!("outer, el: {i}");
-        if i > 10 {
-            println!("middle, el > 10: {i}");
-            if i % 2 == 0 {
-                println!("inner, el % 2: {i}");
-                let i = i + 1;
-                vec.push(i);
-            }
-        }
-    }
-    println!("vec: {vec:?}");
-    println!("### iter: {arr:?} ###");
-    let iter_vec: Vec<_> = arr
-        .iter()
-        .filter(|&i| {
-            println! {"outer, el: {i}"};
-            *i > 10
-        })
-        .filter(|&i| {
-            println!("middle, el > 10: {i}");
-            i % 2 == 0
-        })
-        .map(|i| {
-            println!("inner, el % 2: {i}");
-            i + 1
-        })
-        .collect();
-    assert_eq!(vec, iter_vec);
+    let a = -99_i32;
+    // let a = 9801_f32.sqrt();
+    println!("Hello World: {a}");
+    // let arr: [u32; 8] = [4, 9, 23, 12, 5, 10, 24, 16];
+    // let mut vec = Vec::with_capacity(arr.len());
+    // println!("### for-loop: {arr:?} ###");
+    // for i in arr {
+    // println!("outer, el: {i}");
+    // if i > 10 {
+    // println!("middle, el > 10: {i}");
+    // if i % 2 == 0 {
+    // println!("inner, el % 2: {i}");
+    // let i = i + 1;
+    // vec.push(i);
+    // }
+    // }
+    // }
+    // println!("vec: {vec:?}");
 }
+
+// fn for_loop() {
+// let mut vec = Vec::with_capacity(ARR.len());
+// println!("### for-loop: {ARR:?} ###");
+// for i in ARR {
+// println!("outer, el: {i}");
+// if i > 10 {
+// println!("middle, el > 10: {i}");
+// if i % 2 == 0 {
+// println!("inner, el % 2: {i}");
+// let i = i + 1;
+// vec.push(i);
+// }
+// }
+// }
+// println!("vec: {vec:?}");
+// }
+//
+// fn iter() {
+// println!("### iter: {ARR:?} ###");
+// let iter_vec: Vec<_> = ARR
+// .iter()
+// .filter(|&i| {
+// println! {"outer, el: {i}"};
+// *i > 10
+// })
+// .filter(|&i| {
+// println!("middle, el > 10: {i}");
+// i % 2 == 0
+// })
+// .map(|i| {
+// println!("inner, el % 2: {i}");
+// i + 1
+// })
+// .collect();
+// println!("iter_vec: {iter_vec:?}");
+// }
+//
