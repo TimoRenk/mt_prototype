9f4d2e49741d691d4338077ee55272454cea788a
Timo Renk <timo.renk@gmail.com>
2024-08-29 18:16:32 +0200

    wip: update main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 #![allow(unused)]
 
-// const ARR: [u32; 8] = [4, 9, 23, 12, 5, 10, 24, 16];
+const ARR: [u32; 8] = [4, 9, 23, 15, 5, 10, 24, 16];
 fn main() {
+    let iter_vec: Vec<_> = ARR
+        .iter()
+        .filter(|&i| *i > 9)
+        .filter(|&i| *i < 20)
+        .map(|i| i + 1)
+        .collect();
     let a = -99_i32;
     // let a = 9801_f32.sqrt();
     println!("Hello World: {a}");
+    println!("{}", iter_vec[0]);
     // let arr: [u32; 8] = [4, 9, 23, 12, 5, 10, 24, 16];
     // let mut vec = Vec::with_capacity(arr.len());
     // println!("### for-loop: {arr:?} ###");
@@ -12,7 +19,7 @@ fn main() {
     // println!("outer, el: {i}");
     // if i > 10 {
     // println!("middle, el > 10: {i}");
-    // if i % 2 == 0 {
+    // if ki % 2 == 0 {
     // println!("inner, el % 2: {i}");
     // let i = i + 1;
     // vec.push(i);
@@ -39,23 +46,22 @@ fn main() {
 // println!("vec: {vec:?}");
 // }
 //
-// fn iter() {
-// println!("### iter: {ARR:?} ###");
-// let iter_vec: Vec<_> = ARR
-// .iter()
-// .filter(|&i| {
-// println! {"outer, el: {i}"};
-// *i > 10
-// })
-// .filter(|&i| {
-// println!("middle, el > 10: {i}");
-// i % 2 == 0
-// })
-// .map(|i| {
-// println!("inner, el % 2: {i}");
-// i + 1
-// })
-// .collect();
-// println!("iter_vec: {iter_vec:?}");
-// }
-//
+fn iter() {
+    println!("### iter: {ARR:?} ###");
+    let iter_vec: Vec<_> = ARR
+        .iter()
+        .filter(|&i| {
+            println! {"outer, el: {i}"};
+            *i > 10
+        })
+        .filter(|&i| {
+            println!("middle, el > 10: {i}");
+            i % 2 == 0
+        })
+        .map(|i| {
+            println!("inner, el % 2: {i}");
+            i + 1
+        })
+        .collect();
+    println!("iter_vec: {iter_vec:?}");
+}
